1c3795dd5d1dcc2072462fe8aa5ff29737994c3f
Douglas <douglasd3v.contato@gmail.com>
2026-04-03 19:30:44 -0300

    Adding MAPPER removindo repeated code

--- src/services/UrlService.js
+++ src/services/UrlService.js
@@ -5,12 +5,8 @@ import UrlRepository from "../repositories/UrlRepository.js";
 import AppError from "../errors/AppError.js";
 
 const UrlSchema = z.object({
-  title: z.string().optional(),
   longUrl: z.string().url(),
-  description: z.string().optional(),
 });
-
-const PartialUrlSchema = UrlSchema.partial();
 
 class UrlService {
   async shorten(longUrl, userId) {
@@ -23,39 +19,13 @@ class UrlService {
       userId,
     );
 
-    const { id, longUrl: originalUrl, createdAt, updatedAt } = newUrl.toJSON();
-
-    return {
-      id,
-      originalUrl,
-      shortUrl,
-      createdAt,
-      updatedAt,
-      fullShortUrl: `${process.env.BASE_URL}/${shortUrl}`,
-    };
+    return this.formatUrl(newUrl);
   }
 
   async findAll(userId) {
     const allUrl = await UrlRepository.findAll(userId);
 
-    const urls = allUrl.map((url) => {
-      const {
-        id: urlId,
-        shortUrl,
-        longUrl,
-        createdAt,
-        updatedAt,
-      } = url.toJSON();
-
-      return {
-        urlId,
-        shortUrl,
-        longUrl,
-        createdAt,
-        updatedAt,
-        fullShortUrl: `${process.env.BASE_URL}/${shortUrl}`,
-      };
-    });
+    const urls = allUrl.map((url) => this.formatUrl(url));
 
     return urls;
   }
@@ -67,22 +37,7 @@ class UrlService {
       throw new AppError("Link não encontrado!", 404);
     }
 
-    const {
-      id: urlId,
-      shortUrl,
-      longUrl,
-      createdAt,
-      updatedAt,
-    } = link.toJSON();
-
-    return {
-      urlId,
-      shortUrl,
-      longUrl,
-      createdAt,
-      updatedAt,
-      fullShortUrl: `${process.env.BASE_URL}/${shortUrl}`,
-    };
+    return this.formatUrl(link);
   }
 
   async update(id, data, userId) {
@@ -92,21 +47,13 @@ class UrlService {
       throw new AppError("Link não encontrado!", 404);
     }
 
-    const cleanData = PartialUrlSchema.parse(data);
+    const cleanData = UrlSchema.parse(data);
 
     await UrlRepository.update(id, cleanData, userId);
 
     const newData = await UrlRepository.findById(id, userId);
 
-    const { id: urlId, shortUrl, longUrl, updatedAt } = newData.toJSON();
-
-    return {
-      urlId,
-      shortUrl,
-      longUrl,
-      updatedAt,
-      fullShortUrl: `${process.env.BASE_URL}/${shortUrl}`,
-    };
+    return this.formatUrl(newData);
   }
 
   async delete(id, userId) {
@@ -127,6 +74,19 @@ class UrlService {
     }
 
     return urlData.longUrl;
+  }
+
+  formatUrl(url) {
+    const { id: urlId, shortUrl, longUrl, createdAt, updatedAt } = url.toJSON();
+
+    return {
+      urlId,
+      shortUrl,
+      longUrl,
+      createdAt,
+      updatedAt,
+      fullShortUrl: `${process.env.BASE_URL}/${shortUrl}`,
+    };
   }
 }
 
