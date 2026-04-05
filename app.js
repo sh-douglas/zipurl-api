0b372291330acfc3443937fced8f2c5eb5f10e77
Douglas <douglasd3v.contato@gmail.com>
2026-04-05 08:55:24 -0300

    Cleanup(app.js, packages): Removendo tudo que envolve o Swagger, documentação mal atualizada

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import swaggerUi from "swagger-ui-express";
 
 import { sequelize } from "./src/models/index.js";
 import urlRouter from "./src/routes/urlRoutes.js";
 import userRoutes from "./src/routes/userRoutes.js";
-import swaggerSpec from "./swagger.js";
 import UrlController from "./src/controllers/UrlController.js";
 import globalErrorHandler from "./src/middlewares/GlobalErrorMiddleware.js";
 
@@ -20,7 +18,6 @@ app.use(helmet());
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/v1/auth", userRoutes);
 
 app.use("/api/v1/urls", urlRouter);
